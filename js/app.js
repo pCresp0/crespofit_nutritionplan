9e58d947431d1910807dca7741ba695dc8d7e236
pCrespo <pcbcrespo@gmail.com>
2026-06-17 20:56:48 +0200

    fix: rename 'Leche semi' to 'Leche semidesnatada'

--- js/app.js
+++ js/app.js
@@ -9,7 +9,7 @@ var breakfastOptions = [
     { id:'tostadas', name:'Tostadas', macros:[470,28,52,17],
       items:[{text:'Pan integral trigo/espelta/centeno',amount:120,unit:'g'},{text:'Lomo',amount:50,unit:'g',extra:'+ {30}g queso semi/havarti light',extraBase:30},{text:'Guacamole o aguacate',amount:40,unit:'g'}]},
     { id:'cereales-leche', name:'Cereales con leche y proteína', macros:[430,33,50,13],
-      items:[{text:'Corn flakes / copos de avena / cereales sin azúcar',amount:45,unit:'g'},{text:'Leche semi (un vaso)',amount:200,unit:'ml'},{text:'Whey protein',amount:25,unit:'g'},{text:'Frutos secos / crema de cacahuete',amount:25,unit:'g'}]},
+      items:[{text:'Corn flakes / copos de avena / cereales sin azúcar',amount:45,unit:'g'},{text:'Leche semidesnatada (un vaso)',amount:200,unit:'ml'},{text:'Whey protein',amount:25,unit:'g'},{text:'Frutos secos / crema de cacahuete',amount:25,unit:'g'}]},
     { id:'tortitas', name:'Tortitas de avena', macros:[340,22,30,16],
       items:[{text:'Harina de avena / copos de avena',amount:45,unit:'g'},{text:'2 Huevos + claras',amount:100,unit:'g'},{text:'Frutos secos / crema de cacahuete',amount:10,unit:'g'}]},
     { id:'yogures-proteicos', name:'Yogures proteicos', macros:[420,25,60,8],
